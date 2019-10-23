95bb5effa199fb1e0649488687d6ff3c683ce250
daviemediato <daviemediato@gmail.com>
2019-10-23 10:23:27 -0200

    Criando a relacao com o usuario na main e cadastro do ADM

--- SistemaDeServiços/src/ControleSistemaDeUsuario.java
+++ SistemaDeServiços/src/ControleSistemaDeUsuario.java
@@ -2,98 +2,120 @@
 import java.util.*;
 import java.io.*;
 
-
+//Classe Controle,aqui sera o gerenciador do sistema com diversos metodos
+//auxiliares
 public class ControleSistemaDeUsuario {
-    
-    private ArrayList <Usuario> usuarios;
 
+    //Atributos da classe de Controle
+    private ArrayList<Usuario> usuarios;
+
+    //Construtor classe de Controle
     public ControleSistemaDeUsuario(String diretorio) throws IOException {
         usuarios = new ArrayList<>();
         lerDados(diretorio);
     }
-       
-    public void cadastraUsuario(Usuario novo){
-        usuarios.add(novo);
-    }
 
+    //Getters e Setters para a classe de Controle
     public ArrayList<Usuario> getUsuarios() {
         return usuarios;
     }
-    
-    private void lerDados(String caminho) throws FileNotFoundException, IOException{
-        
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    //Aqui sao duas funcoes de cadostro: uma para os usuarios "normais",ou seja,
+    //clientes e funcionários
+    public void cadastraUsuario(Usuario novoUser) {
+        usuarios.add(novoUser);
+    }
+
+    //Outra para o Administrador,apenas adms cadastram adms
+    public boolean verificaAdministrador(String id_adm, String senha_adm) {
+        return (id_adm.equals("admin") && senha_adm.equals("admin"));
+    }
+
+    public void cadastraAdministrador(Administrador novoAdm) {
+
+        usuarios.add(novoAdm);
+    }
+
+    //Metodo que le os dados de um arquivo passado como parametro,conforme o modelo
+    private void lerDados(String caminho) throws FileNotFoundException, IOException {
+
         BufferedReader br = new BufferedReader(new FileReader(caminho));
-        
+
         String nome;
         String endereco;
         String email;
         String telefone;
         String id_usuario;
         String senha_usuario;
-        
-        String dadoLido; 
-        
-        while(br.ready()){
-            //NOME
+
+        String dadoLido;
+
+        while (br.ready()) {
+
+            //Nome
             dadoLido = br.readLine();
             nome = dadoLido;
-            
-            //ADDR
+
+            //Endereco
             dadoLido = br.readLine();
             endereco = dadoLido;
-            
-            //EMAIL
+
+            //Email
             dadoLido = br.readLine();
             email = dadoLido;
-            
-            //TEL
+
+            //Telefone
             dadoLido = br.readLine();
             telefone = dadoLido;
-            
-            //ID
+
+            //Id_usuario
             dadoLido = br.readLine();
             id_usuario = dadoLido;
-            
-            //SENHA
+
+            //Senha_usuario
             dadoLido = br.readLine();
             senha_usuario = dadoLido;
-            
-            //TIPO
+
+            //Tipo da classe (Adm,Cliente,Profissinal)
             dadoLido = br.readLine();
             String tipo = dadoLido;
-            
-            //ESPAÇO VAZIO
+
+            //Espaco vazio, caracteriza que ja lemos um "card" de usuario
             dadoLido = br.readLine();
-            
-            
-            switch(tipo){
+
+            //Aqui acontece a logica de insercao
+            switch (tipo) {
                 case "Administrador":
-                    usuarios.add(new Administrador( nome,  endereco,  email,  telefone,  id_usuario, senha_usuario));
+                    usuarios.add(new Administrador(nome, endereco, email, telefone, id_usuario, senha_usuario));
                     break;
-                
+
                 case "Cliente":
-                    usuarios.add(new Cliente( nome,  endereco,  email,  telefone,  id_usuario, senha_usuario));
+                    usuarios.add(new Cliente(nome, endereco, email, telefone, id_usuario, senha_usuario));
                     break;
-                
+
                 case "Profissional":
-                    usuarios.add(new Profissional( nome,  endereco,  email,  telefone,  id_usuario, senha_usuario));
+                    usuarios.add(new Profissional(nome, endereco, email, telefone, id_usuario, senha_usuario));
                     break;
-                    
+
                 default:
                     System.out.println("Usuario não encontrado");
                     break;
             }
-            
-            
+
         }
         br.close();
     }
-    
-    private void escreverDados(String caminho) throws IOException{
+
+    //Metodo que escreve os dados em um arquivo conforme o modelo
+    private void escreverDados(String caminho) throws IOException {
         FileWriter arq = new FileWriter(caminho);
         PrintWriter gravarArq = new PrintWriter(arq);
-        
-        for(Usuario usuario : usuarios){
+
+        for (Usuario usuario : usuarios) {
             gravarArq.println(usuario.getNome());
             gravarArq.println(usuario.getEndereco());
             gravarArq.println(usuario.getEmail());
@@ -101,14 +123,20 @@ public class ControleSistemaDeUsuario {
             gravarArq.println(usuario.getId_usuario());
             gravarArq.println(usuario.getSenha_usuario());
             gravarArq.println(usuario.getNome());
-            gravarArq.println();    
+            gravarArq.println();
         }
-        
+
         arq.close();
     }
-    
-    public void finalizarPrograma(String diretorio) throws IOException{//AQUI A GENTE EXECUTA TUDO QUE É NECESSÁRIO PARA FECHAR O PROGRAMA CORRETAMENTE 
+
+    //Aqui a gente executa tudo que é necessário para fechar o programa corretamente
+    public void finalizarPrograma(String diretorio) throws IOException {
         escreverDados(diretorio);
     }
     
+    //Aqui a gente executa tudo que é necessario para iniciar o programa corretamente
+    public void iniciarPrograma(String diretorio) throws IOException {
+        lerDados(diretorio);
+    }
+
 }
--- SistemaDeServiços/src/TesteSistemaDeServicos.java
+++ SistemaDeServiços/src/TesteSistemaDeServicos.java
@@ -1,14 +1,153 @@
 
-import java.io.IOException;
-
+import java.io.*;
+import java.util.*;
 
 public class TesteSistemaDeServicos {
 
-
     public static void main(String[] args) throws IOException {
-        
-        ControleSistemaDeUsuario controleUsuario = new ControleSistemaDeUsuario(args[0]);
-        
+
+        //Aqui instanciamos uma "unidade" de controle do usuario e lemos o arquivo de texto
+        ControleSistemaDeUsuario controleUsuario = new ControleSistemaDeUsuario("dados.txt");
+        controleUsuario.iniciarPrograma("dados.txt");
+        //utilizar args[0] ?
+
+        //Aqui instaniamos variaveis de auxilio
+        Scanner dadoLido = new Scanner(System.in);
+        int opcao_usuario, opcao2_usuario;
+        String nome_usuario;
+        String id_usuario;
+        String senha_usuario;
+        String telefone_usuario;
+        String endereco_usuario;
+        String email_usuario;
+        String id_adm;
+        String senha_adm;
+
+        //Aqui iniciaremos a relacao com o usuario do sistema
+        do {
+
+            System.out.println("Bem vindo !");
+            System.out.println("Selecione sua opcao:");
+            System.out.println("[1] Login");
+            System.out.println("[2] Cadastrar");
+            System.out.println("[3] Recuperar Senha");
+            System.out.println("[4] Redefinir Senha");
+            System.out.println("[5] Sair");
+            opcao_usuario = dadoLido.nextInt();
+            
+            switch (opcao_usuario) {
+                
+                //Caso login
+                case 1:
+                    System.out.println("Digite seu nome de usuario: ");
+                    id_usuario = dadoLido.next();
+                    System.out.println("Digite sua senha do usuario: ");
+                    senha_usuario = dadoLido.next();
+                    //chama determinado metodo nao implementado
+                    break;
+
+                //Caso cadastramento
+                case 2:
+                    System.out.println("Bem vindo ao processo de cadastramento");
+                    System.out.println("Selecione sua opcao:");
+                    System.out.println("[1] Cadastrar Cliente");
+                    System.out.println("[2] Cadastrar Profissinal");
+                    System.out.println("[2] Cadastrar Administrador");
+                    opcao2_usuario = dadoLido.nextInt();
+
+                    switch (opcao2_usuario) {
+
+                        //Caso cliente
+                        case 1:
+                            System.out.println("Digite seu nome completo: ");
+                            nome_usuario = dadoLido.next();
+                            System.out.println("Digite seu endereco: ");
+                            endereco_usuario = dadoLido.next();
+                            System.out.println("Digite seu email: ");
+                            email_usuario = dadoLido.next();
+                            System.out.println("Digite seu telefone: ");
+                            telefone_usuario = dadoLido.next();
+                            System.out.println("Digite seu nome de usuario: ");
+                            id_usuario = dadoLido.next();
+                            System.out.println("Digite sua senha: ");
+                            senha_usuario = dadoLido.next();
+                            Usuario cliente = new Cliente(nome_usuario, endereco_usuario, email_usuario, telefone_usuario, id_usuario, senha_usuario);
+                            controleUsuario.cadastraUsuario(cliente);
+
+                            break;
+
+                        //Caso profissional
+                        case 2:
+                            System.out.println("Digite seu nome completo: ");
+                            nome_usuario = dadoLido.next();
+                            System.out.println("Digite seu endereco: ");
+                            endereco_usuario = dadoLido.next();
+                            System.out.println("Digite seu email: ");
+                            email_usuario = dadoLido.next();
+                            System.out.println("Digite seu telefone: ");
+                            telefone_usuario = dadoLido.next();
+                            System.out.println("Digite seu nome de usuario: ");
+                            id_usuario = dadoLido.next();
+                            System.out.println("Digite sua senha: ");
+                            senha_usuario = dadoLido.next();
+
+                            Usuario profissional = new Profissional(nome_usuario, endereco_usuario, email_usuario, telefone_usuario, id_usuario, senha_usuario);
+                            controleUsuario.cadastraUsuario(profissional);
+
+                            break;
+
+                        //Caso administrador
+                        case 3:
+
+                            System.out.println("Digite seu id de administrador: ");
+                            id_adm = dadoLido.next();
+                            System.out.println("Digite sua senha de administrador: ");
+                            senha_adm = dadoLido.next();
+
+                            if (controleUsuario.verificaAdministrador(id_adm, senha_adm)) {
+                                System.out.println("Digite seu nome completo: ");
+                                nome_usuario = dadoLido.next();
+                                System.out.println("Digite seu endereco: ");
+                                endereco_usuario = dadoLido.next();
+                                System.out.println("Digite seu email: ");
+                                email_usuario = dadoLido.next();
+                                System.out.println("Digite seu telefone: ");
+                                telefone_usuario = dadoLido.next();
+                                System.out.println("Digite seu nome de usuario: ");
+                                id_usuario = dadoLido.nextLine();
+                                System.out.println("Digite sua senha: ");
+                                senha_usuario = dadoLido.next();
+
+                                Usuario adm = new Administrador(nome_usuario, endereco_usuario, email_usuario, telefone_usuario, id_usuario, senha_usuario);
+                                controleUsuario.cadastraUsuario(adm);
+                            } else {
+                                System.out.println("Voce nao possui permissao para cadastrar Administrador");
+                            }
+
+                            break;
+                    }
+
+                    break;
+
+                //Caso Recuperar senha
+                case 3:
+                    break;
+
+                //Caso Redefinir senha 
+                case 4:
+                    break;
+
+                //Caso de saida    
+                case 5:
+                    //Aqui finalizamos o programa e salvamos todas as alteracoes feitas
+                    controleUsuario.finalizarPrograma("dados.txt");
+                    break;
+                
+                //Caso default
+                default:
+                    System.out.println("Nao existe tal possibilidade");
+            }
+
+        } while (opcao_usuario != 5);
     }
-    
 }
